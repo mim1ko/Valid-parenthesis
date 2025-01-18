8abb4e41b6e143568c27b41322b2d3cd76f2859c
mim1ko <rsl.mmmd@gmail.com>
2025-01-18 15:08:08 +0400

    Update SolutionWithoutStack.cpp

--- SolutionWithoutStack.cpp
+++ SolutionWithoutStack.cpp
@@ -1,6 +1,10 @@
+#include <string>
+#include <iostream>
+
+
 class SolutionWithoutStack {
 public:
-    bool isValid(string s) {
+    bool isValid(std::string s) {
         int length = 0;
 
         length = s.size();
@@ -39,45 +43,47 @@ public:
         int sum_among = 0;
         int sum = 0;
 
-        
-            for (int i{}; i < length - 1; i++) {
-                sum = sum + array[i];
-                if (array[i] > 0) {
-                    for (int k{i + 1}; k < length; k = k + 2) {
-                        if (array[i] == -array[k]) {
-                            correct = true;
-                            break;
-                        }else{
-                            correct = false;
-                        }
-                    }
-                } if (correct == false) {
-                    return false;
-                }
-                for (int j{i + 1}; j < length; j = j + 2) {
-                    if (array[i] == -array[j]) {
-                        for (int k{i}; k <= j; k++) {
-                            sum_among = sum_among + array[k];
-                        }
-                        if (sum_among == 0 && array[i] > array[j]) {
-                            sum_id = true;
-                        } else {
-                            sum_id = false;
-                            break;
-                        }
-                    }
-                    sum_among = 0;
-                    if (sum_id == true && (j - i) == 1) {
+
+        for (int i{}; i < length - 1; i++) {
+            sum = sum + array[i];
+            if (array[i] > 0) {
+                for (int k{ i + 1 }; k < length; k = k + 2) {
+                    if (array[i] == -array[k]) {
+                        correct = true;
                         break;
                     }
-                    if (sum_id == false) {
+                    else {
+                        correct = false;
+                    }
+                }
+            } if (correct == false) {
+                return false;
+            }
+            for (int j{ i + 1 }; j < length; j = j + 2) {
+                if (array[i] == -array[j]) {
+                    for (int k{ i }; k <= j; k++) {
+                        sum_among = sum_among + array[k];
+                    }
+                    if (sum_among == 0 && array[i] > array[j]) {
+                        sum_id = true;
+                    }
+                    else {
+                        sum_id = false;
                         break;
                     }
                 }
-               
-                
-            }sum = sum + array[length-1];
-        
+                sum_among = 0;
+                if (sum_id == true && (j - i) == 1) {
+                    break;
+                }
+                if (sum_id == false) {
+                    break;
+                }
+            }
+
+
+        }sum = sum + array[length - 1];
+
         if (sum_id == false || sum != 0) {
             return false;
         }
